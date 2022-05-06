a829a857f838490fd68b8de54823e0ce655b33cd
Yous42 <youssef.zaidi.42@gmail.com>
2022-05-06 12:53:57 +0200

    form inscription et modif

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Form\AccountType;
 use App\Repository\AccountRepository;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +18,59 @@ class AccountController extends AbstractController
     public function __construct(private AccountRepository $accountRepository)
     {
         
+    }
+
+    // Attention à l'ordre des functions => si je la met dessous la fonction index, c'est la fonction index qui sera appelé car elle comment par utilisateur/et le slug comme il n'est pas défini peut être remplacé par nouveau => ne va pas su la bonne page
+
+    // Ajouter new utilisateur
+    #[Route('/utilisateur/nouveau', name: 'app_account_new')]
+    public function newAccount(Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(AccountType::class, new Account());
+        $form->handleRequest($request);
+
+        if ($form -> isSubmitted() && $form->isValid()) {
+            /** @var Account $account */
+            $account = $form -> getData();
+            $account -> setCreatedAt(new DateTime());
+            $account -> setSlug($account->getName());
+            $em -> persist($account);
+            $em -> flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('account/newAccount.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    // Modifier utilisateur
+    #[Route('/utilisateur/modifier/{slug}', name: 'app_account_edit')]
+    public function editAccount(Account $account, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(AccountType::class, $account);
+        $form->handleRequest($request);
+
+        if ($form -> isSubmitted() && $form->isValid()) {
+            /** @var Account $account */
+            $account -> setSlug($account->getName());
+            $em -> flush();
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('account/editAccount.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route('/utilisateur/comptes', name: 'app_account_all')]
+    public function getAllAccounts(AccountRepository $accountRepository): Response
+    {
+        return $this->render('account/allAccount.html.twig', [
+            'allAccounts' => $accountRepository->findAll()
+        ]);
     }
 
     #[Route('/utilisateur/{name}', name: 'app_account_details')]
@@ -32,6 +91,7 @@ class AccountController extends AbstractController
             'accountDetails' => $this -> accountRepository -> getAccountDetails($name),
         ]);
     }
+
 
 
 }
---
+++ src/Form/AccountType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Nom',
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Email',
+                ]
+            ])
+            ->add('nickname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Pseudo',
+                ]
+            ])
+            // ->add('slug', TextType::class, [
+            //     'label' => false,
+            //     'attr' => [
+            //         'placeholder' => 'Slug',
+            //     ]
+            // ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Soumettre',
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
